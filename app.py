15d5b48b0b5b1fcbac9fce5ddcfc5c3e04ee5fc0
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-01-26 18:50:51 +0530

    changes in app.py fro not responding

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
+# Download 'punkt' if not available
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
 
 def transform_text(text):
     text = text.lower()
@@ -40,15 +45,22 @@ st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
+    
+    # Display the processed text
+    st.subheader("Processed Text:")
+    st.write(transformed_sms)
+
     # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
+    
     # 3. predict
     result = model.predict(vector_input)[0]
-    # 4. Display
+
+    # 4. Display prediction result
+    st.subheader("Prediction:")
     if result == 1:
-        st.header("Spam")
+        st.write("Spam")
     else:
-        st.header("Not Spam")
+        st.write("Not Spam")
